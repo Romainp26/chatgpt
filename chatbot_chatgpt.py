85b3f27dd792016c6b6106fad9c0fa62ad250f8a
Romainp26 <51722137+Romainp26@users.noreply.github.com>
2025-02-13 18:41:38 +0700

    Add files via upload

--- chatbot_chatgpt.py
+++ chatbot_chatgpt.py
@@ -7,8 +7,8 @@ import openai
 # configuring openai - api key
 working_dir = os.path.dirname(os.path.abspath(__file__))
 config_data = json.load(open(f"{working_dir}/config.json"))
-st.secrets["OpenAI_key"] == "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"
-openai.api_key = st.secrets["OpenAI_key"]
+#st.secrets["OpenAI_key"] == "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"
+openai.api_key = st.secrets['openai']["OPENAI_API_KEY"]
 
 # configuring streamlit page settings
 st.set_page_config(
